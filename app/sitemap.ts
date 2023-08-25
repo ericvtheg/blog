17f5fc8b43140ff00fec2a92d3ad1f4478ce8bc3
Eric Ventor <ericventor97@gmail.com>
2023-08-25 13:55:50 -0700

    sitemap now includes landing page and about page

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,5 +2,16 @@ import { MetadataRoute } from 'next';
 import { allPosts } from 'contentlayer/generated';
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  return allPosts.map((post) => ({url: `https://ericventor.com${post.slug}`, lastModified: post.date}));
+  const posts = allPosts.map((post) => ({url: `https://ericventor.com${post.slug}`, lastModified: post.date}));
+  return [
+    ...posts,
+    {
+      url: 'https://ericventor.com',
+      lastModified: new Date().toISOString(),
+    },
+    {
+      url: 'https://ericventor.com/about',
+      lastModified: new Date().toISOString(),
+    }
+  ];
 }
