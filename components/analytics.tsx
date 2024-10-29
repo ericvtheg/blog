c9481411f2b7591c18f8351f00c4d1892131da32
Eric Ventor <ericventor97@gmail.com>
2024-10-28 19:57:27 -0700

    feat: include google analytics

--- components/analytics.tsx
+++ components/analytics.tsx
@@ -1,7 +1,34 @@
 "use client";
 
 import { Analytics as VercelAnalytics } from "@vercel/analytics/react";
+import Script from "next/script";
+
+function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      ></Script>
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+      </Script>
+    </>
+  );
+}
 
 export function Analytics() {
-  return <VercelAnalytics />;
+  return (
+    <>
+      <VercelAnalytics />
+      <GoogleAnalytics />
+    </>
+  );
 }
+
+const GA_TRACKING_ID = "G-5YSEFPTG53";
